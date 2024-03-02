f85ff03fbcdb585a7fce9eeca6079bb804723c13
Tempiusz <zanczakjakub@gmail.com>
2024-03-02 23:47:18 +0100

    Unfinished welcome page

--- Algorithmic/base_app/views.py
+++ Algorithmic/base_app/views.py
@@ -1,3 +1,7 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.template import loader
 
-# Create your views here.
+def welcome_site(request):
+    template = loader.get_template('welcome.html')
+    return HttpResponse(template.render())
